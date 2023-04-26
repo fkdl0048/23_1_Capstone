0a02dcfc54dd286a4483dea2dba6104961320369
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-04-27 03:41:44 +0900

    Feat: Title Done.

--- CapStone/Assets/Scripts/Runtime/Managers/UIManager.cs
+++ CapStone/Assets/Scripts/Runtime/Managers/UIManager.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class UIManager
 {
+    private WarningToolTip _warningToolTip;
+    private const string WarningToolTipPath = "Prefabs/UI/WarningToolTip";
+    public void Init()
+    {
+        _warningToolTip = GameManager.Resource.Load<WarningToolTip>(WarningToolTipPath);
+        _warningToolTip = GameObject.Instantiate(_warningToolTip);
+        _warningToolTip.gameObject.SetActive(false);
+    }
     
+    public void SetWarningText(string warningText)
+    {
+        _warningToolTip.SetWarningText(warningText);
+        _warningToolTip.gameObject.SetActive(true);
+    }
 }
