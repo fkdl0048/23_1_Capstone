5bf4f2127bc8126f6184e990b4ed07f2d3ac6b58
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-04-28 12:55:45 +0900

    Feat: ToolTip Separation

--- CapStone/Assets/Scripts/Runtime/Managers/UIManager.cs
+++ CapStone/Assets/Scripts/Runtime/Managers/UIManager.cs
@@ -1,21 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager
 {
     private WarningToolTip _warningToolTip;
+    private NotiToolTip _notiToolTip;
+    
     private const string WarningToolTipPath = "Prefabs/UI/WarningToolTip";
+    private const string NotiToolTipPath = "Prefabs/UI/NotiToolTip";
+
     public void Init()
     {
         _warningToolTip = GameManager.Resource.Load<WarningToolTip>(WarningToolTipPath);
         _warningToolTip = GameObject.Instantiate(_warningToolTip);
         _warningToolTip.gameObject.SetActive(false);
+
+        _notiToolTip = GameManager.Resource.Load<NotiToolTip>(NotiToolTipPath);
+        _notiToolTip = GameObject.Instantiate(_notiToolTip);
+        _notiToolTip.gameObject.SetActive(false);
     }
     
     public void SetWarningText(string warningText)
     {
         _warningToolTip.SetWarningText(warningText);
         _warningToolTip.gameObject.SetActive(true);
+    }
+    
+    public void SetNotiText(string notiText)
+    {
+        _notiToolTip.SetNotiText(notiText);
+        _notiToolTip.gameObject.SetActive(true);
     }
 }
