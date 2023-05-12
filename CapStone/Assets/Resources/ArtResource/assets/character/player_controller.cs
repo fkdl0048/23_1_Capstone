88f1c1f89bb174bd8050060dcf7ee18eb4fc279e
LJI028 <83956520+LJI028@users.noreply.github.com>
2023-05-12 14:05:27 +0900

    test

--- CapStone/Assets/Resources/ArtResource/assets/character/player_controller.cs
+++ CapStone/Assets/Resources/ArtResource/assets/character/player_controller.cs
@@ -8,6 +8,15 @@ public class player_controller : MonoBehaviour
 {   
     [SerializeField]
     private float speed = 5f; // 이동 속도
+    [SerializeField]
+    private GameObject tree;
+    [SerializeField]
+    private GameObject water;
+    [SerializeField]
+    private GameObject farm;
+    [SerializeField]
+    private GameObject plantGenerator;
+
     private float xMove, yMove;
     private bool isThereTree = false; // 근처에 벌목할 수 있는 나무가 있는지
     private bool isThereWater = false; // 근처에 낚시가능한 물이 있는지
@@ -21,14 +30,9 @@ public class player_controller : MonoBehaviour
     private int random = 0;
     private int ptCount = 0; // 식물 개수
     private int nowPlant = -1; // 현재 관리하는 식물
-
-    private GameObject tree;
-    private GameObject grid;
-    private GameObject water;
-    private GameObject farm;
+  
     private GameObject[] plants = new GameObject[3];
-    private GameObject plantGenerator;
-
+    
     private Rigidbody2D rb;
     private Animator animator;
     private SpriteRenderer sprite;
@@ -40,21 +44,26 @@ public class player_controller : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         this.animator = GetComponent<Animator>();
-        this.tree = GameObject.Find("tree");
-        this.grid = GameObject.Find("Grid");
-        this.water = grid.transform.Find("water").gameObject;
-        this.farm = grid.transform.Find("farm").gameObject;
-        this.plantGenerator = GameObject.Find("plantGenerator");
+        //this.tree = GameObject.Find("tree");
+        //this.grid = GameObject.Find("Grid");
+        //this.water = grid.transform.Find("water").gameObject;
+        //this.farm = grid.transform.Find("farm").gameObject;
+        //this.plantGenerator = GameObject.Find("plantGenerator");
         ptCount = 0;
+
+        PV = GetComponent<PhotonView>();
         
     }
 
     void Update()
     {
-        Move();
-        Logging();
-        Fishing();
-        Farming();
+        if (PV.IsMine)
+        {
+            Move();
+            Logging();
+            Fishing();
+            Farming();
+        }
     }
 
     void OnTriggerStay2D(Collider2D collision)
@@ -100,36 +109,35 @@ public class player_controller : MonoBehaviour
     {
         if (nowFishing) // 현재 낚시중이면 이동 불가
             return;
-        //if (PV.IsMine)
-        // {
-        xMove = 0;
-        yMove = 0;
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            xMove = speed * Time.deltaTime;
-            sprite.flipX = false;
-            animator.SetBool("isMoving", true);
+            xMove = 0;
+            yMove = 0;
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                xMove = speed * Time.deltaTime;
+                flipXRPC(false);
+                animator.SetBool("isMoving", true);
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            xMove = -speed * Time.deltaTime;
-            sprite.flipX = true;
-            animator.SetBool("isMoving", true);
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                xMove = -speed * Time.deltaTime;
+                flipXRPC(true);
+                animator.SetBool("isMoving", true);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            yMove = speed * Time.deltaTime;
-            animator.SetBool("isMoving", true);
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                yMove = speed * Time.deltaTime;
+                animator.SetBool("isMoving", true);
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            yMove = -speed * Time.deltaTime;
-            animator.SetBool("isMoving", true);
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                yMove = -speed * Time.deltaTime;
+                animator.SetBool("isMoving", true);
         }
 
-        if (!(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))) // 키를 누르지 않은 상태면 이동 애니메이션 중지
-            animator.SetBool("isMoving", false);
+            if (!(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))) // 키를 누르지 않은 상태면 이동 애니메이션 중지
+                animator.SetBool("isMoving", false);
 
         this.transform.Translate(new Vector3(xMove, yMove, 0));
       
@@ -244,7 +252,16 @@ public class player_controller : MonoBehaviour
                 plants[nowPlant].GetComponent<Animator>().SetBool("harvest", true);
             }
     }
+
+    [PunRPC]
+    void flipXRPC(bool flip)
+    {
+
+        sprite.flipX = flip;
+            
+    }
 }
+
 
 
 
