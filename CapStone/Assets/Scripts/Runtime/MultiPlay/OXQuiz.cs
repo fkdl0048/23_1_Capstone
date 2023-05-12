9a1eb26275b5478b8d2bc5567c177eadb095d8a3
MSLEE <leems041625@gmail.com>
2023-05-12 16:27:50 +0900

    [Feat] : OX 퀴즈 스크립트 생성

---
+++ CapStone/Assets/Scripts/Runtime/MultiPlay/OXQuiz.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class OXQuiz : PlayerManager
+{
+    #region PublicVariables
+    #endregion
+
+    #region PrivateVariables
+    #endregion
+
+    #region PublicMethod
+    #endregion
+
+    #region PrivateMethod
+    #endregion
+}
