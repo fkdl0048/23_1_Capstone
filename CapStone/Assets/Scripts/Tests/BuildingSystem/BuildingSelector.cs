cd0a66efc7a43f544330887b5590099e14e5a338
MSLEE <leems041625@gmail.com>
2023-05-23 16:33:20 +0900

    [Feat] : pull 전 커밋

--- CapStone/Assets/Scripts/Tests/BuildingSystem/BuildingSelector.cs
+++ CapStone/Assets/Scripts/Tests/BuildingSystem/BuildingSelector.cs
@@ -1,35 +1,35 @@
-//using System.Collections.Generic;
-//using BuildingSystem.Models;
-//using UnityEngine;
-//using UnityEngine.InputSystem;
-//using GameInput;
+using System.Collections.Generic;
+using BuildingSystem.Models;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using GameInput;
 
-//namespace BuildingSystem
-//{
-//    public class BuildingSelector : MonoBehaviour
-//    {
-//        [SerializeField]
-//        private List<BuildableItem> _buildables;
+namespace BuildingSystem
+{
+    public class BuildingSelector : MonoBehaviour
+    {
+        [SerializeField]
+        private List<BuildableItem> _buildables;
 
-//        [SerializeField]
-//        private BuildingPlacer _buildingPlacer;
+        [SerializeField]
+        private BuildingPlacer _buildingPlacer;
 
-//        private int _activeBuildableIndex;
+        private int _activeBuildableIndex;
 
-//        private void OnEnable()
-//        {
-//            InputActions.Instance.Player.NextItem.performed += OnNextItemPerformed;
-//        }
+        private void OnEnable()
+        {
+            InputActions.Instance.Player.NextItem.performed += OnNextItemPerformed;
+        }
 
-//        private void OnNextItemPerformed(InputAction.CallbackContext ctx)
-//        {
-//            NextItem();
-//        }
+        private void OnNextItemPerformed(InputAction.CallbackContext ctx)
+        {
+            NextItem();
+        }
 
-//        private void NextItem()
-//        {
-//            _activeBuildableIndex = (_activeBuildableIndex + 1) % _buildables.Count;
-//            _buildingPlacer.SetActiveBuildable(_buildables[_activeBuildableIndex]);
-//        }
-//    }
-//}
+        private void NextItem()
+        {
+            _activeBuildableIndex = (_activeBuildableIndex + 1) % _buildables.Count;
+            _buildingPlacer.SetActiveBuildable(_buildables[_activeBuildableIndex]);
+        }
+    }
+}
